ac74224ce4cefa74b4f90aa6322f0234b4cfd049
unknown <jyotizannatul@gmail.com>
2024-07-02 16:12:17 +0600

    v1.0.1

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# version 1.0.1
+
 import subprocess
 import os
 import json
@@ -10,7 +12,11 @@ import win32con
 from win10toast import ToastNotifier
 
 def show_message_box(message, title):
-    ctypes.windll.user32.MessageBoxW(0, message, title, 0x40 | 0x1)
+    try:
+        ctypes.windll.user32.MessageBoxW(0, message, title, 0x40 | 0x1)
+    except:
+        print("There was an in show_message_box")
+    
 
 def push_windows_notification(title, message):
     toaster = ToastNotifier()
@@ -28,7 +34,7 @@ def HubstaffTracking(filePath):
     
     # Find the start and end of the JSON part
     json_start_index = mixed_text.find('{')
-    json_end_index = mixed_text.rfind('}') + 1  # rfind to find the last occurrence, +1 to include the '}'
+    json_end_index = mixed_text.rfind('}') + 1
 
     # Extract the JSON part
     json_str = mixed_text[json_start_index:json_end_index]
@@ -42,26 +48,6 @@ def HubstaffTracking(filePath):
     except json.JSONDecodeError:
         print("Error: Invalid JSON format in output")  
 
-def run_hubstaff(hubstaffShortcutPath):
-    try:
-        if platform.system() == "Windows":
-            # Start Hubstaff
-            os.startfile(hubstaffShortcutPath)
-            # Wait for the application to fully open (adjust time as needed)
-            time.sleep(5)
-            # Find the window handle and bring it to the foreground
-            app_window = win32gui.FindWindow(None, "Hubstaff")  # Adjust window title if necessary
-            if app_window:
-                win32gui.ShowWindow(app_window, win32con.SW_RESTORE)  # Restore the window if minimized                
-                win32gui.SetForegroundWindow(app_window)  # Bring the window to the foreground                
-            else:
-                print("Hubstaff window not found.")
-        else:
-            print("Currently, only Windows is supported for direct shortcut opening.")
-    except Exception as e:
-        push_windows_notification('Hubstaff Tracking Off', "Your hubstaff tracking is turned off")
-        print(f"Failed to start or bring Hubstaff to foreground: {e}")  
-
 def main():
     with open('settings.json', 'r') as file:
         data = json.load(file)
@@ -70,12 +56,10 @@ def main():
     screenLock           = data.get('screenLock', '')    
     popUpWindow          = data.get('popUpWindow', '')    
     popUpNotification    = data.get('popUpNotification', '')    
-    delay                = data.get('delay', '')    
-    hubstaffShortcutPath = data.get('hubstaffShortcutPath', '')    
+    delay                = data.get('delay', '')
 
     false_count = 0
-    alert_threshold = delay  # 5 minutes in seconds
-    # alert_threshold = 5 * 60  # 5 minutes in seconds
+    alert_threshold = delay
     start_time = time.time()
     
     while True:
@@ -86,21 +70,16 @@ def main():
         else:
             false_count += 1
             
-            # Check if more than 5 minutes have passed
+            # Check if more than alert_threshold minutes have passed
             if false_count * 1 > alert_threshold:
                 elapsed_time = time.time() - start_time
                 print(f"Alert! Hubstaff tracking is off)")
 
                 if popUpNotification == 1:
-                    push_windows_notification('Hubstaff Tracking Off', "Your hubstaff tracking is turned off")
-
-                if popUpWindow == 1:
-                    run_hubstaff(hubstaffShortcutPath)
+                    push_windows_notification('Hubstaff Tracking Off', "Your hubstaff tracking is turned off")                
 
                 if screenLock == 1:
-                    ctypes.windll.user32.LockWorkStation()
-
-                # show_message_box("Hubstaff client is not active. Please start the application.", "Hubstaff Alert")
+                    ctypes.windll.user32.LockWorkStation()                
 
                 false_count = 0  # Reset false count after alert
         
